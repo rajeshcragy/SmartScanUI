5ff4aa4a043cc06121a4442856ec3b71aedbd390
Rajesh B <rajeshcragy@outlook.com>
2026-02-20 12:03:23 +0530

    scanner class and dlls are added

---
+++ SmartScanUI/Scanner/CZUR/CzurEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartScanUI.Scanner.CZUR
+{
+    public class CzurEngine
+    {
+        public CzurEngine()
+        {
+            
+        }
+
+
+    }
+}
